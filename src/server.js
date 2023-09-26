e9d89c92e63b2a0393f7677a89066a046dbfbb67
Mariana Jager <mariana.macedosoares@hotmail.com>
2023-09-26 11:41:20 -0300

    adicionei o nodemon e iniciei um app.get com route params

--- src/server.js
+++ src/server.js
@@ -1,7 +1,24 @@
-import express from 'express'; // ou const express = require('express')
+import express from 'express';
+
 const app = express();
 
 const porta = 3333;
 
-app.listen(porta, ()=> console.log('servidor carregado'));
+app.get('/message/:id/:user',(request,response)=>{
+   const {id,user} = request.params; // desestruturação
+
+   response.send(`Uau! Você solicitou informações do usuário ${id}, que está registrado com o nome de usuário: ${user}`);
+});
+
+
+
+app.get('/',(request,response)=>{
+    response.send("Respondendo a uma requisição")  
+})
+
+app.listen(porta,()=> console.log(`servidor ativado na porta ${porta}`));
+
+
+
+
 
